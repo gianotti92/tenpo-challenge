8be3a9e6088eff667ca8f0aefc99b4b513638245
Lucas <gianotti.92@gmail.com>
2023-05-05 12:03:44 -0300

    initial-commit
    creation of empty repository

---
+++ src/main/java/com/challenge/tenpo/TenpoApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.tenpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenpoApplication.class, args);
+	}
+
+}
