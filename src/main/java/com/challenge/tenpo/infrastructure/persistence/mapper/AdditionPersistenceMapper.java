327bbf9be420070022b149d5e0fc13fa24ef821a
Lucas <gianotti.92@gmail.com>
2023-05-06 13:11:25 -0300

    update gitignore

--- src/main/java/com/challenge/tenpo/infrastructure/persistence/mapper/AdditionPersistenceMapper.java
+++ src/main/java/com/challenge/tenpo/infrastructure/persistence/mapper/AdditionPersistenceMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 
-@Mapper()
+@Mapper
 public interface AdditionPersistenceMapper {
     @Mapping(target = "firstAddend", source = "firstAddend")
     @Mapping(target = "secondAddend", source = "secondAddend")
