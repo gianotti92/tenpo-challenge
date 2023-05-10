0f8104df65206ad9464a274ccae9891d4ff29e24
Lucas Gianotti <gianotti.92@gmail.com>
2023-05-10 08:26:35 -0300

    Adds rate limit

--- src/main/java/com/challenge/tenpo/infrastructure/controller/CalculatorController.java
+++ src/main/java/com/challenge/tenpo/infrastructure/controller/CalculatorController.java
@@ -5,6 +5,10 @@ import com.challenge.tenpo.infrastructure.controller.dto.AdditionDto;
 import com.challenge.tenpo.infrastructure.controller.dto.ExternalCallDto;
 import com.challenge.tenpo.infrastructure.controller.mapper.AdditionControllerMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,37 +17,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/v1/adding")
+@RequestMapping("/v1/addition")
 public class CalculatorController {
 
   private AdditionService additionService;
 
   private AdditionControllerMapper additionControllerMapper;
 
+  private final Bucket bucket;
+  
   public CalculatorController(AdditionService additionService, AdditionControllerMapper additionControllerMapper) {
     this.additionService = additionService;
     this.additionControllerMapper = additionControllerMapper;
+
+    Bandwidth limit = Bandwidth.simple(3, Duration.ofMinutes(1));
+    this.bucket = Bucket.builder()
+            .addLimit(limit)
+            .build();
   }
 
   @PostMapping
   public ResponseEntity<AdditionDto> create(@RequestBody AdditionDto additionDto) throws JsonProcessingException {
-    var addition = additionControllerMapper.map(additionDto);
-    additionService.calculateAddition(addition);
-    return ResponseEntity.ok().build();
+    if (bucket.tryConsume(1)) {
+      var addition = additionControllerMapper.map(additionDto);
+      additionService.calculateAddition(addition);
+      return ResponseEntity.ok().build();
+    }else {
+      return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+    }
+    
   }
 
   @GetMapping(params = { "page", "size" })
   public ResponseEntity<List<ExternalCallDto>> get(@RequestParam("page") Integer page,
                                                    @RequestParam("size") Integer size) {
-    var response = additionService.getAdditions(page, size).stream()
-            .map(additionControllerMapper::map)
-            .collect(Collectors.toList());
+    if (bucket.tryConsume(1)) {
+      var response = additionService.getAdditions(page, size).stream()
+              .map(additionControllerMapper::map)
+              .collect(Collectors.toList());
+
+      return ResponseEntity.ok(response);
+    }else {
+      return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+    }
     
-    return ResponseEntity.ok(response);
   }
 
 }
